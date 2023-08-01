8c2c9e17a85bfee7fe9b443c435441646fb99b03
root-14 <ilkaygavaz2@gmail.com>
2023-08-02 01:33:56 +0300

    add mainScreen Ui

--- app/src/main/java/com/root14/mstock/ui/LoginGoogleFragment.kt
+++ app/src/main/java/com/root14/mstock/ui/LoginGoogleFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.snackbar.Snackbar
 import com.root14.mstock.R
 import com.root14.mstock.databinding.FragmentLoginGoogleBinding
 
@@ -21,8 +23,7 @@ class LoginGoogleFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginGoogleBinding.inflate(inflater, container, false)
         return binding.root
@@ -31,9 +32,19 @@ class LoginGoogleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Glide.with(view)
-            .load(R.drawable.day_google_500)
+        Glide.with(view).load(R.drawable.day_google_500)
             .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
             .into(binding.LoginGoogleIconImageView)
+
+
+        binding.loginGoogleButton.setOnClickListener {
+
+            Snackbar.make(binding.root, "demo-login", Snackbar.LENGTH_SHORT).show()
+
+            //TODO: implement firebase googleAuth
+
+            binding.root.findNavController()
+                .navigate(R.id.action_loginGoogleFragment_to_mainScreenFragment)
+        }
     }
 }
--- app/src/main/java/com/root14/mstock/ui/MainScreenFragment.kt
+++ app/src/main/java/com/root14/mstock/ui/MainScreenFragment.kt
@@ -1,23 +1,35 @@
 package com.root14.mstock.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.root14.mstock.R
+import com.root14.mstock.databinding.FragmentMainScreenBinding
 
 class MainScreenFragment : Fragment() {
+    private var _binding: FragmentMainScreenBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.extendedFab.setOnClickListener {
+            //TODO:implement barcode reader
+        }
     }
 }
